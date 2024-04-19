93428a52fb2130dce1d54eaf2572722392fca0ff
Rudra Sharma <94120092+rudra016@users.noreply.github.com>
2024-04-19 22:01:10 +0530

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-openai_api_key = "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"
-corpus_api_key = "zut_sXqeAjKFcCC5pIc5M9Uj593DT6NmSAppYLneXg"
-together_api_key = "a6df272782f4d10330817dc3c81f03135da14115778cdf575d5f98d3d2baed4c"
